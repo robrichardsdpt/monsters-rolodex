8f39d42303dbe55b31e868db6f01808bbb555ba6
Rob <robrichardsdpt@gmail.com>
2020-11-27 15:03:18 -0500

    can filter monsters

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,8 +3,9 @@ import './card.styles.css'
 
 export const Card = (props) => (
     <div className='card-container'>
-        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2`}/>
-        <h1>{props.monster.name}</h1>
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x280`}/>
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
     </div>
 )
 
