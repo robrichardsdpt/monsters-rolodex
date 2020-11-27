8b1090ac60cf712d642a4a8bd158a27e0ff9c08f
Rob <robrichardsdpt@gmail.com>
2020-11-27 13:55:55 -0500

    set up grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component }from 'react'
 import './App.css'
+import CardList from './components/card-list/card-list.component.jsx'
 
 class App extends Component {
   constructor() {
@@ -18,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-           {this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)}
+      <CardList>{this.state.monsters.map(monster => <h1 key={monster.id}> { monster.name } </h1>)}</CardList>
       </div>
     );
    }
